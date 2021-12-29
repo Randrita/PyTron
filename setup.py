5ff970e8390e2c63d8d87c2657e8f2e8a1f25eb4
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2021-12-29 13:21:14 +0530

    setup finished

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+    PROJECT_NAME = "PyTron"
+    USER_NAME= "Randrita"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="randritas@gmail.com",
+    description="Small implementation of perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Randrita/PyTron",
+    project_urls={
+        "Bug Tracker": "https://github.com/Randrita/PyTron/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "tqdm"
+    ]
+)
